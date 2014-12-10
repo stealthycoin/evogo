bf6197c7cf5d40819e151a3c3c8f6081a9b8f4de
stealthycoin <john.w.carlyle@gmail.com>
2014-12-10 12:17:57 -0800

    Improved hello world example comments

--- hello_world/main.go
+++ hello_world/main.go
@@ -24,11 +24,13 @@ func CreateGene() evo.Gene {
 	}
 }
 
+// Define a function for mutating a gene, in our case it will simply make a new one
 func MutateGene(g evo.Gene) evo.Gene {
 	return CreateGene()
 }
 
-// Function to display an individual
+// Function to display an individual (optional),
+// if provided each generation will show the highest fitness individual
 func ShowGenes(i *evo.Individual) {
 	for _, g := range i.Genes() {
 		gene := g.(Gene)
@@ -36,7 +38,8 @@ func ShowGenes(i *evo.Individual) {
 	}
 }
 
-// Define a
+// Define a fitness function to evaluate an individual
+// In our case we are just using distance between unicode runes
 func fitness(i *evo.Individual) int {
 	var f int = 0 // Highest score it can get is 0
 	for i, g := range i.Genes() {
@@ -57,7 +60,7 @@ func main() {
 	// 13 Genes per individual
 	// And our custom CreateGene function used to make the genes
 	pop := evo.NewPopulation(1000, 13, CreateGene)
-	pop.SetShowIndividual(ShowGenes)
+	pop.SetShowIndividual(ShowGenes) // Give it our show function
 
 	// Call the train function and provide it with
 	// The population to train
